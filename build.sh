82c4bf217b2b2089e4b30e01e3fa5a3a063a59ff
Christopher Horrell <christopher@horrell.ca>
2022-03-22 19:03:34 -0400

    ci: check shell scripts (#107)
    Also: format build.sh

--- build.sh
+++ build.sh
@@ -4,45 +4,45 @@ set -euo pipefail
 IFS=$'\n\t'
 
 usage() {
-    cat <<USAGE
+  cat << USAGE
 Usage: $0 -n NODE_VERSION
     -n <NODE_VERSION> (Required)
     -h help
 Example:
     $0 -n 16.2.0
 USAGE
-    exit 1
+  exit 1
 }
 
 NODE_VERSION=
 
 while getopts n:h? options; do
-    case ${options} in
+  case ${options} in
     n)
-        NODE_VERSION=${OPTARG}
-        ;;
+      NODE_VERSION=${OPTARG}
+      ;;
     h)
-        usage
-        ;;
+      usage
+      ;;
     \?)
-        usage
-        ;;
-    esac
+      usage
+      ;;
+  esac
 done
 
 if [[ -z ${NODE_VERSION} ]]; then
-    echo "FATAL: No value for -n, NODE_VERSION"
-    echo ""
-    usage
+  echo "FATAL: No value for -n, NODE_VERSION"
+  echo ""
+  usage
 fi
 
 NODE_KEYS=$(curl -fsSLo- --compressed https://github.com/nodejs/node/raw/master/README.md | awk '/^gpg --keyserver hkps:\/\/keys\.openpgp\.org --recv-keys/ {print $NF}')
 
 for key in $NODE_KEYS; do
-    if [[ -n "$key" ]]; then
-        gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || \
-        gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ;
-    fi
+  if [[ -n "$key" ]]; then
+    gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" ||
+      gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key"
+  fi
 done
 
 curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz"
@@ -53,7 +53,7 @@ tar -Jxf "node-v$NODE_VERSION.tar.xz"
 cd "node-v$NODE_VERSION/"
 ./configure --fully-static --enable-static --without-npm --without-intl
 # See: https://github.com/nodejs/node/issues/41497#issuecomment-1013137433
-for i in out/tools/v8_gypfiles/gen-regexp-special-case.target.mk out/test_crypto_engine.target.mk; do 
-    sed -i 's/\-static//g' $i || echo "nevermind";
+for i in out/tools/v8_gypfiles/gen-regexp-special-case.target.mk out/test_crypto_engine.target.mk; do
+  sed -i 's/\-static//g' $i || echo "nevermind"
 done
 make -j"$(getconf _NPROCESSORS_ONLN)"
