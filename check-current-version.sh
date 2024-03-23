7457c94d96f54aa1026a209c083ce5f5d364a581
Christopher Horrell <christopher@horrell.ca>
2024-03-23 11:16:04 -0400

    Output NODE_VERSION and MAJOR_VERSION

--- check-current-version.sh
+++ check-current-version.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-LATEST_NODE_VERSION=$(curl -fsSLo- --compressed https://nodejs.org/dist/index.json | jq '.[1].version')
+NODE_VERSION=$(curl -fsSLo- --compressed https://nodejs.org/dist/index.json | jq '.[1].version' | tr -d '"' | tr -d 'v')
+MAJOR_VERSION=$(echo "${NODE_VERSION}" | cut -d'.' -f 1)
 
-# Check for specific tag based on LATEST_NODE_VERSION
-if ! docker manifest inspect chorrell/node-minimal:"${LATEST_NODE_VERSION#v}" > /dev/null 2>&1;
+# Check for specific tag based on NODE_VERSION
+if ! docker manifest inspect chorrell/node-minimal:"${NODE_VERSION}" > /dev/null 2>&1;
 then
-  echo "${LATEST_NODE_VERSION#v}"
-  else
-  echo "BUILT"
+  echo "NODE_VERSION=$NODE_VERSION"
+  echo "MAJOR_VERSION=$MAJOR_VERSION"
 fi
